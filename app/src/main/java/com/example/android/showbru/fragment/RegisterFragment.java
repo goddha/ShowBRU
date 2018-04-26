f4553d36151ab7fe8df2d8b1b9315ff503349d23
goddha <goddha@outlook.com>
2018-04-26 10:36:01 +0700

    Replace Fragment Success

--- app/src/main/java/com/example/android/showbru/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/android/showbru/fragment/RegisterFragment.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.android.showbru.MainActivity;
 import com.example.android.showbru.R;
@@ -24,9 +28,49 @@ public class RegisterFragment extends Fragment {
 
     }   // Main Method
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemUpload) {
+
+            uploadValueToServer();
+            return true;
+
+        }
+    }
+
+    private void uploadValueToServer() {
+//        get value from EditText
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+//        Change Data Type From EditText to String
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim(); //trim = no space
+
+//        Check Space
+
+        if (nameString.isEmpty() || userString.isEmpty() || passwordString.isEmpty()) // false or false or false /have space {
+        } else // no space {
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
+
     private void createToolbar() {
         Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
 
 //        Setup Title
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
@@ -34,15 +78,18 @@ public class RegisterFragment extends Fragment {
 
 
 //        Setup Navigation Icon
-((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
-((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        getActivity().getSupportFragmentManager().popBackStack();
-    }
-});
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+        setHasOptionsMenu(true);
+
 
     }
 
